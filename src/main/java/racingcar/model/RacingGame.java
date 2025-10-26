e17844d9354c783d7c6ad5341c667f995a013ea9
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-27 08:42:02 +0900

    refactor : 공백제거

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -22,16 +22,14 @@ public class RacingGame {
     }
     public String winner(List<Car> cars){
         int maxPosition = cars.stream()
-                .mapToInt(Car::getDistance) // 각 차의 position 값(int)을 가져옴
-                .max()                    // 그중 최대값을 찾음
-                .orElse(0);               // 리스트가 비어있으면 0을 반환
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
 
-        // 2. 최대 위치와 같은 차(들)만 필터링한 후,
-        //    그 차들의 이름을 가져와서 쉼표(,)로 연결합니다.
         String winners = cars.stream()
-                .filter(car -> car.getDistance() == maxPosition) // 최대 위치를 가진 차만 통과
-                .map(Car::getName)                               // 차(Car)를 이름(String)으로 변경
-                .collect(Collectors.joining(","));             // 이름들을 쉼표로 합침
+                .filter(car -> car.getDistance() == maxPosition)
+                .map(Car::getName)
+                .collect(Collectors.joining(","));
         return winners;
     }
     private void moveCar(Car car){
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,8 +2,6 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import racingcar.play.Application;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
