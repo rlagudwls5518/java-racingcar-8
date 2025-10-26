55f0be6eb10c620fc5105311e6db0c225053d3d0
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-27 08:39:17 +0900

    feat(Exception): 예외메시지 상수클래스 생성

---
+++ src/main/java/racingcar/model/exception/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package racingcar.model.exception;
+
+public class ExceptionMessage {
+
+    public static final String NAME_CANNOT_BE_BLANK ="이름은 공백만으로 구성될 수 없습니다.";
+    public static final String NAME_EXCEEDS_MAX_LENGTH = "이름은 5자 이하만 가능합니다: ";
+    public static final String TRY_COUNT_MUST_BE_NUMBER = "시도 횟수는 숫자만 입력 가능합니다: ";
+    public static final String TRY_COUNT_CANNOT_BE_BLANK = "시도 횟수는 공백일 수 없습니다.";
+}
