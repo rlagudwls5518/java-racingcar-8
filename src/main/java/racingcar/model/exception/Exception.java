3c61207d85300aa9f5f6586bd18457f7359de912
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-27 08:40:01 +0900

    refactor(Exception): 예외메시지 매개변수 수정

--- src/main/java/racingcar/model/exception/Exception.java
+++ src/main/java/racingcar/model/exception/Exception.java
@@ -3,24 +3,21 @@ package racingcar.model.exception;
 public class Exception {
 
     public void exceptionCarNameCheck(String name){
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("이름은 null 또는 공백일 수 없습니다.");
-        }
         if (name.trim().isEmpty()) {
-            throw new IllegalArgumentException("이름은 공백만으로 구성될 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.NAME_CANNOT_BE_BLANK);
         }
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다: " + name);
+            throw new IllegalArgumentException(ExceptionMessage.NAME_EXCEEDS_MAX_LENGTH + name);
         }
     }
     public void exceptionTryCountCheck(String count){
         try {
             Integer.parseInt(count);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자만 입력 가능합니다: " + count);
+            throw new IllegalArgumentException(ExceptionMessage.TRY_COUNT_MUST_BE_NUMBER + count);
         }
         if (count.isBlank()) {
-            throw new IllegalArgumentException("시도 횟수는  공백일 수 없습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.TRY_COUNT_CANNOT_BE_BLANK);
         }
     }
 
