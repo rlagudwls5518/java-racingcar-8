ee52c64706837fbfb53b096ac3d75d34fd0884f7
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-27 13:45:59 +0900

    refactor : 출력메시지 상수화 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,10 +5,10 @@ import racingcar.model.Car;
 
 public class OutputView extends Printer {
 
-    private final String INPUT_PROMPT_CARNAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String INPUT_PROMPT_TRYCOUNT = "시도할 횟수는 몇 회인가요?";
-    private final String OUTPUT_PROMPT_WINNER = "최종 우승자 : ";
-    private final String OUTPUT_PROMPT_DISTANCE = "-";
+    private static final String INPUT_PROMPT_CARNAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_PROMPT_TRYCOUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String OUTPUT_PROMPT_WINNER = "최종 우승자 : ";
+    private static final String OUTPUT_PROMPT_DISTANCE = "-";
 
     public void printReadyMessage() {
         println(INPUT_PROMPT_CARNAME);
