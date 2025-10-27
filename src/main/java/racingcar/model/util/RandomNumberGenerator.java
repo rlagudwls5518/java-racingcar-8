c84efc729f6e900891ca520817e2cd2b18916cce
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-27 16:30:32 +0900

    refactor : 랜덤 범위 변수명 수정

--- src/main/java/racingcar/model/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/model/util/RandomNumberGenerator.java
@@ -3,11 +3,11 @@ package racingcar.model.util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
-    private final static int ZERO = 0;
-    private final static int NINE = 9;
+    private final static int FIRST_NUMBER = 0;
+    private final static int LAST_NUMBER = 9;
 
     public static int randomNumber() {
-        return Randoms.pickNumberInRange(ZERO, NINE);
+        return Randoms.pickNumberInRange(FIRST_NUMBER, LAST_NUMBER);
 
     }
 }
